5e916a78bcf19c0ea3fd4985db16851c729178e8
Judah M. Ritterman <judah29@gmail.com>
2016-05-15 12:33:43 -0500

    beautify

--- full-game.js
+++ full-game.js
@@ -1,58 +1,57 @@
-function iChingGame(){
-//Generate Hexagram
-  //generate six lines
+function iChingGame() {
+    //Generate Hexagram
+    //generate six lines
     //return six lines (full hexagram)
 
 
 
 
-////////////////////////////////////////////////////
-// Generate Line
-  // The Coin Toss
+    ////////////////////////////////////////////////////
+    // Generate Line
+    // The Coin Toss
     // Toss three coins
     // each coin tossed has value of 3 (heads) or 2 (tails)
 
 
-// flip one coin and return a value of 3 (heads) or 2 (tails)
-function flipOneCoin(){
-   return Math.round( Math.random() ) + 2;
-}
+    // flip one coin and return a value of 3 (heads) or 2 (tails)
+    function flipOneCoin() {
+        return Math.round(Math.random()) + 2;
+    }
 
-// flip 3 coins and return a value of 6, 7, 8, 9
-function tossResult(){
-var totalNumber = 0;
+    // flip 3 coins and return a value of 6, 7, 8, 9
+    function tossResult() {
+        var totalNumber = 0;
 
-  for (var i = 1; i <= 3; i++) {
-    totalNumber+= flipOneCoin();
-  }
-  return(totalNumber);
-}
+        for (var i = 1; i <= 3; i++) {
+            totalNumber += flipOneCoin();
+        }
+        return (totalNumber);
+    }
 
 
-function convertNumberToLine(){
-  var tossOutcome = tossResult();
-  if( tossOutcome % 2 === 0 ){
-    return("____   ____");
-  }
-  else{
-    return("___________");
-  }
+    function convertNumberToLine() {
+        var tossOutcome = tossResult();
+        if (tossOutcome % 2 === 0) {
+            return ("____   ____");
+        } else {
+            return ("___________");
+        }
 
-}
+    }
 
-function generateHexagram(){
+    function generateHexagram() {
 
-var i = 1;
-var hexString = "";
-while(i <= 6){
-  hexString+= convertNumberToLine() + "\n";
-  i++;
-}
-return(hexString);
+        var i = 1;
+        var hexString = "";
+        while (i <= 6) {
+            hexString += convertNumberToLine() + "\n";
+            i++;
+        }
+        return (hexString);
 
-}
+    }
 
-console.log(generateHexagram());
+    console.log(generateHexagram());
 
 
 };
